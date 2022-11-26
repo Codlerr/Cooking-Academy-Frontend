a31f0a22278d411950ef3575706df35f03ebae4b
Codlerr <codlerr.online@gmail.com>
2022-11-26 17:09:20 +0530

    first committ

--- src/pages/landing/landingPage.jsx
+++ src/pages/landing/landingPage.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import '../landing/landingPage.css';
-import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms';
 
 function LandingPage() {
   return (
     <>
-    <AccessAlarmsIcon/>
-    <div className='h-[40vh]  grid place-items-center '>
-        <h1 className='xs:text-yellow-600 sm:text-blue-600 md:text-pink-600 lg:text-green-600 xl:text-red-600 text-xl font-bold'>hais</h1>
+    <div className='h-screen  grid place-items-center '>
+    <i class="fa-solid fa-fan animate-spin xs:text-yellow-600 sm:text-blue-600 md:text-pink-600 lg:text-green-600 xl:text-red-600 text-6xl"></i>
     </div>
     </>
   )
