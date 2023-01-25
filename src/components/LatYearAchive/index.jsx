a0955c17564cc01844c783bd287a7c21f9ace1b8
Codlerr <codlerr.online@gmail.com>
2023-01-25 16:31:49 +0530

    LSA count effect

--- src/components/LatYearAchive/index.jsx
+++ src/components/LatYearAchive/index.jsx
@@ -1,8 +1,13 @@
+import React,{useState} from 'react';
 import CountUp from 'react-countup';
+import ScrollTrigger from 'react-scroll-trigger';
 import './styles.css';
 
 
-export default function lsa(props) {
+export default function LSA(props) {
+
+  const [counterOn, setCounterOn] = useState(false)
+
   return (
     <>
       <div data-aos="fade-right">
@@ -10,24 +15,36 @@ export default function lsa(props) {
         {/* <p className='xs:w-[90%] md:w-[80%] mx-auto text-center text-[#ffffffcb] xs:text-lg md:text-xl py-5'>we are a culinary startup based in qatar with the aim of bringing people together, to learn and enjoy the art of cooking.</p> */}
       </div>
 
+      <ScrollTrigger onEnter={() => setCounterOn(true)} onExit={() => setCounterOn(false)}>
       <div data-aos="fade-right" className='md:w-10/12 mx-auto md:pt-5 grid grid-cols-1 md:grid-cols-4 xs:gap-5 md:gap-0 place-items-center text-[color:var(--thm-clr2)] py-2'>
         <div className='grid place-items-center'>
+          {counterOn &&
           <h4 className='xs:text-5xl lg:text-7xl'>+<CountUp start={0} end={25} /></h4>
+          }
           <p className='capitalize'>Partners</p>
         </div>
         <div className='grid place-items-center'>
+        {counterOn &&
           <h4 className='xs:text-5xl lg:text-7xl'>+<CountUp start={0} end={50}/></h4>
+        }
           <p className='capitalize'>Workshop</p>
         </div>
         <div className='grid place-items-center'>
+        {counterOn &&
           <h4 className='xs:text-5xl lg:text-7xl'>+<CountUp start={0} end={3}/></h4>
+        }
           <p className='capitalize'>awards</p>
         </div>
         <div className='grid place-items-center'>
+        {counterOn &&
           <h4 className='xs:text-5xl lg:text-7xl'>+<CountUp start={0} end={150}/></h4>
+        }
           <p className='capitalize'>cooking class</p>
         </div>
 
-      </div></>
+      </div>
+      </ScrollTrigger>
+      
+      </>
   )
 }
